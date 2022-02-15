fdef18eb27ca1a59f1cd4c784f1026f9ba74bb80
Jamie Smith <thejamiemsmith@gmail.com>
2022-02-15 16:47:34 -0700

    completed prob 16

--- lab.js
+++ lab.js
@@ -379,14 +379,24 @@ const shippingInfo = {
 */
 
 //Code Here 
-
+class Vehicle {
+  constructor(capacity, color, mileage){
+    this.capacity = capacity
+    this.color = color
+    this.mileage = mileage
+  }
+  move(miles) {
+    this.mileage = this.mileage + miles
+    console.log(this.mileage)
+  }
+}
 
 /*
   Create a vehicle using your new class and save it to a variable called myFirstVehicle
 */
 
 //Code Here
-
+let myFirstVehicle = new Vehicle(5, "black", 150000)
 
 /* 
   Now we'll create a class that's based off of the vehicle class. 
@@ -397,17 +407,21 @@ const shippingInfo = {
 */
 
 //Code Here
-
+class Motorcycle extends Vehicle {
+  constructor(capacity, color, mileage, make, isCool) {
+    super(capacity, color, mileage)
+  }
+}
 /*
   Create a Motorcycle using your new class and save it to a variable called myFirstMotorcycle
 */
 
 //Code Here 
-
+let myFirstMotorcycle = new Motorcycle (2, "red", 250, "Yamaha", true)
 /*
   Call the move function on myFirstMotorcycle (don't forget the parameter)
 */
-
+// myFirstMotorcycle.move(75)
 /*
   Let's make another class based off of Vehicle. 
 
@@ -425,7 +439,24 @@ const shippingInfo = {
 */
 
 //Code Here
-
+class Boat extends Vehicle {
+  constructor(capacity, color, mileage, name, type, isSeaworthy) {
+    super(capacity, color, mileage)
+    this.name = name
+    this.type = type
+    this.isSeaworthy = isSeaworthy
+  }
+  checkSeaworthiness(){
+    if (this.isSeaworthy === true) {
+      console.log(`The ${this.color} ${this.type} ${this.name} is seaworthy!`)
+    } else {
+      console.log(`You need to get your ${this.type} in shape!`)
+    }
+  }
+  performMaintenance() {
+    this.isSeaworthy = true
+  }
+}
 
 /*
   Create a new boat using your class. You can choose whatever values you like for all the 
@@ -433,21 +464,22 @@ const shippingInfo = {
 */
 
 //Code Here
-
+let myFirstBoat = new Boat(8, "blue", 2600, "Usain Boat", "speed boat", false)
 /*
   Call the checkSeaworthiness method on your new boat
 */
-
 //Code Here
+// myFirstBoat.checkSeaworthiness()
 
 /*
   Now run the performMaintenance method on your boat
 */
 
 //Code Here 
-
+myFirstBoat.performMaintenance()
 /*
   Check the seaworthiness once more (you should be ready for the water!)
 */
 
 //Code Here
+// myFirstBoat.checkSeaworthiness()
